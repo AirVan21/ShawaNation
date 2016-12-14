48692076f4cc9db4f13f357f2285d764972f7903
semionn <mrsemionn@gmail.com>
2016-12-14 18:41:23 +0300

    hotfix

--- sentiment_analysis/start_sentiment_service.sh
+++ sentiment_analysis/start_sentiment_service.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo dockebuild . -t sentiment
+sudo docker build Docker -t sentiment
 sudo docker run -p 5001:5000 -v $(pwd)/Docker/Volume:/root/Volume sentiment "/root/main.py"
